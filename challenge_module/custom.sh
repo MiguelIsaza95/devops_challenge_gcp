e49eeb797c5c4ddc1c465fe507da13ca289aa108
MiguelIsaza95 <miguel11andres@hotmail.com>
2022-07-29 20:54:09 +0000

    store tfstate remotley

--- challenge_module/custom.sh
+++ challenge_module/custom.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Install ansible
-sudo yum install git -y
+sudo yum update -y
+sudo yum install -y git
 sudo yum install -y yum-utils
-sudo dnf install ansible-core -y
+sudo dnf install -y ansible-core
 ansible --version
 
 # Configure environment for ansible local provision
-cd $HOME
 git clone https://github.com/MiguelIsaza95/devops_challenge_ansible.git
 cd devops_challenge_ansible
 
